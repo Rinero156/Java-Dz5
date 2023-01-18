a944d94636be181fdb0831ae530601a7e1d8d707
Konstantin S <97kostada@gmail.com>
2023-01-18 19:03:47 +0300

    Attempt 2

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,17 +2,14 @@ package ru.netology.sqr;
 
 public class SQRService {
     public int searchForSquares(int beginningRange, int endRange) {
-        int i = 10;
-        int y = 0;
-       do {
-           i++;
-           if (beginningRange <= i * i && i * i <= endRange) {
-               y++;
-           }
-           } while (i <= 99); {
-           return y;
+        int squareCountCounter = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (beginningRange <= i * i && i * i <= endRange) {
+                squareCountCounter++;
+            }
         }
+        return squareCountCounter;
 
-       }
     }
+}
 
